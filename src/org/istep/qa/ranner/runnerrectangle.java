06fa4b8059ecb45214023cedbdaee217df86f888
Laiman <Laiman2004@gmail.com>
2018-10-13 12:10:14 +0300

    Initial commit

---
+++ src/org/istep/qa/ranner/runnerrectangle.java
@@ -0,0 +1,12 @@
+package org.istep.qa.ranner;
+
+import org.istep.qa.lesson.rectangle;
+
+public class runnerrectangle {
+    public static void main(String[] args) {
+        rectangle rectangle = new rectangle(1,2,3,3);
+        rectangle rectangle1 = new rectangle(4,4);
+        rectangle rectangle2 = new rectangle();
+
+    }
+}
